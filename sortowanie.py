03dd060169b169e7e37255b1303736f077de49c6
Kajiysor <mrbarman55@gmail.com>
2021-10-17 00:25:16 +0200

    changed sortowanie

--- sortowanie.py
+++ sortowanie.py
@@ -1,8 +1,6 @@
 from random import randint
 import matplotlib.pyplot as plt
 
-# Ilosc liczb w tablicy
-#ARR_SIZE = 120
 
 # Tablica liczb
 A = []
@@ -48,20 +46,20 @@ def insertionSort(arr):
         arr[j+1] = x
     return [il_por, il_przes]
 
-randomizeArray(A, 1000)
-#print("Nieposortowana tablica: ", A)
+randomizeArray(A, 100)
+print("Nieposortowana tablica: ", A)
 
 out1 = bubbleSort(A)
-#print("Bubble Sort wynik: ", A)
+print("Bubble Sort wynik: ", A)
 print("Ilosc operacji porownania: ", out1[0])
 print("Ilosc operacji przestawienia: ", out1[1])
 
 print("\n\n")
-randomizeArray(A, 1000)
-#print("Nieposortowana tablica: ", A)
+randomizeArray(A, 100)
+print("Nieposortowana tablica: ", A)
 
 out2 = insertionSort(A)
-#print("Insertion Sort wynik: ", A)
+print("Insertion Sort wynik: ", A)
 print("Ilosc operacji porownania: ", out2[0])
 print("Ilosc operacji przestawienia: ", out2[1])
 
